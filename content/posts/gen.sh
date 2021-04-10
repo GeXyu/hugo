5f272d18b36b0be31d5751d890e79e10a42c3986
gexiuyu <861719405@qq.com>
2021-04-10 15:36:06 +0800

    .

---
+++ content/posts/gen.sh
@@ -0,0 +1,12 @@
+rm -rf template.md
+touch template.md
+
+echo '---' > template.md
+echo 'title: ' >> template.md
+echo "date: $(date +%Y-%m-%d\T%H:%M:%S\+08:00)" >> template.md
+echo "lastmod: $(date +%Y-%m-%d\T%H:%M:%S\+08:00)" >> template.md
+echo 'draft: false                       # 是否是草稿' >> template.md
+echo 'tags:                              # 标签['','']' >> template.md
+echo 'categories:                        # 分类 ['','']' >> template.md
+echo 'author: "real_ge"                  # 作者' >> template.md
+echo '---' >> template.md
