f4040c61a30095483edb00b09fba730ba2b13379
gexiuyu <861719405@qq.com>
2021-04-27 18:05:55 +0800

    .

--- content/posts/2021/2021_04_26_english_index.md
+++ content/posts/2021/2021_04_26_english_index.md
@@ -12,4 +12,6 @@ author: "ge"                  # 作者
 [简单句基本句型1](https://blog.tnt.pub/2021_04_25_english_simple_grammar_01.html)
 [简单句基本句型2](https://blog.tnt.pub/2021_04_26_english_simple_grammar_02.html)
 ## 谓语动词
+[时态概述](https://blog.tnt.pub/2021_04_27_english_tense_overview.html)
+
 ...未完待续
--- content/posts/2021/gen.sh
+++ content/posts/2021/gen.sh
@@ -5,7 +5,7 @@ echo '---' > template.md
 echo 'title: ' >> template.md
 echo "date: $(date +%Y-%m-%d\T%H:%M:%S\+08:00)" >> template.md
 echo "lastmod: $(date +%Y-%m-%d\T%H:%M:%S\+08:00)" >> template.md
-echo 'draft: true                       # 是否是草稿' >> template.md
+echo 'draft: false                       # 是否是草稿' >> template.md
 echo 'tags:                              # 标签['','']' >> template.md
 echo 'categories:                        # 分类 ['','']' >> template.md
 echo 'author: "ge"                  # 作者' >> template.md
